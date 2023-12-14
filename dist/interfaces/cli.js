5794a9da589159e4d3b4302ca7c8690138b6b294
nazimbo <113207575+nazimbo@users.noreply.github.com>
2023-12-14 11:29:19 +0100

    Add console log and update task in CLI interface

--- dist/interfaces/cli.js
+++ dist/interfaces/cli.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // src/interfaces/cli.ts
 const Task_1 = require("../domain/Task");
 const InMemoryTaskRepository_1 = require("../adapters/InMemoryTaskRepository");
+console.log("Backend connected");
 const taskRepository = new InMemoryTaskRepository_1.InMemoryTaskRepository();
 const createTaskUseCase = {
     execute: (task) => taskRepository.create(task),
@@ -11,6 +12,15 @@ const getTasksUseCase = {
     execute: () => taskRepository.getTasks(),
 };
 // Exemple d'utilisation
+// Création d'une tâche
 const newTask = new Task_1.Task("1", "Nouvelle tâche", "Description", new Date(), false);
 createTaskUseCase.execute(newTask);
+console.log("Tâches:", getTasksUseCase.execute());
+// Mise à jour d'une tâche
+const taskToUpdate = taskRepository.getTasks()[0];
+taskToUpdate.title = "Tâche mise à jour";
+const updateTaskUseCase = {
+    execute: (task) => taskRepository.update(task),
+};
+updateTaskUseCase.execute(taskToUpdate);
 console.log("Tâches:", getTasksUseCase.execute());
--- src/interfaces/cli.ts
+++ src/interfaces/cli.ts
@@ -2,7 +2,11 @@
 import { Task } from "../domain/Task";
 import { CreateTask } from "../usecases/CreateTask";
 import { GetTasks } from "../usecases/GetTasks";
+import { UpdateTask } from "usecases/UpdateTask";
+import { DeleteTask } from "usecases/DeleteTask";
 import { InMemoryTaskRepository } from "../adapters/InMemoryTaskRepository";
+
+console.log("Backend connected");
 
 const taskRepository = new InMemoryTaskRepository();
 const createTaskUseCase: CreateTask = {
@@ -13,6 +17,16 @@ const getTasksUseCase: GetTasks = {
 };
 
 // Exemple d'utilisation
+// Création d'une tâche
 const newTask = new Task("1", "Nouvelle tâche", "Description", new Date(), false);
 createTaskUseCase.execute(newTask);
+console.log("Tâches:", getTasksUseCase.execute());
+
+// Mise à jour d'une tâche
+const taskToUpdate = taskRepository.getTasks()[0];
+taskToUpdate.title = "Tâche mise à jour";
+const updateTaskUseCase: UpdateTask = {
+  execute: (task: Task) => taskRepository.update(task),
+};
+updateTaskUseCase.execute(taskToUpdate);
 console.log("Tâches:", getTasksUseCase.execute());
